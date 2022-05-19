8e5d16657fc751be39373833efe3963aeb68e557
Ahmed Tarek <ahmedshabana@paymob.com>
2022-05-19 16:47:10 +0200

    first commit

---
+++ Example/Pods/Target Support Files/ATFramework/ATFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ATFramework : NSObject
+@end
+@implementation PodsDummy_ATFramework
+@end
---
+++ fuck.swift
@@ -0,0 +1,8 @@
+//
+//  fuck.swift
+//  ATFramework
+//
+//  Created by Ahmed Tarek on 19/05/2022.
+//
+
+import Foundation
